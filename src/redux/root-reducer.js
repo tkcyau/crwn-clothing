cda704565faa5271c9e470f0ae336ba4bc418c2c
Tim <tkcyau@gmail.com>
2020-04-18 12:25:04 -0400

    split off state to collections overview

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,23 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
-export default combineReducers({
+import directoryReducer from "../redux/directory/directory.reducer";
+import shopReducer from "../redux/shop/shop.reducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
