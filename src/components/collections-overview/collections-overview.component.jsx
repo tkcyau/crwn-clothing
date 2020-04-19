0e6918268bfb77e25976952a675cad46f44fa455
Tim <tkcyau@gmail.com>
2020-04-19 10:15:37 -0400

    changed shop data into bobjects

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 import "./collections-overview.styles.scss";
 
@@ -15,6 +15,6 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStatetoProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 export default connect(mapStatetoProps)(CollectionsOverview);
