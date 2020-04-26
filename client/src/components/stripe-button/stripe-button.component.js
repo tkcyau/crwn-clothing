fc6772b347b4e545aa055b57519a772a3884a5b7
Tim <tkcyau@gmail.com>
2020-04-25 23:31:01 -0400

    moved front end to client and set up backend for stripe payment

---
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,43 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_DfNH6VK7V422LC0Lh5NFDkxn00acN4P0b3";
+
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", error);
+        alert(
+          "There was an issue with your payment.  Please make sure to use accepted credit cards."
+        );
+      });
+  };
+  return (
+    <StripeCheckout
+      label="Pay now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.sv"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
